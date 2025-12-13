8373a033ebf303e47a99d990abb5ce4d0880d259
BaptLR <baba1408.mail@gmail.com>
2025-12-13 15:55:02 +0100

    update to V1

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import ipaddress
 
 AMAZON_URL = "https://ip-ranges.amazonaws.com/ip-ranges.json"
 OUTPUT_FOLDER = "target"
@@ -22,21 +23,53 @@ def extract_prefixes(data):
     return prefixes
 
 
-def write_prefixes(prefixes, output_file):
+def expand_ips(prefixes):
+    all_ips = set()
+    print("[+] Expanding CIDR to individual IPs...")
+
+    for cidr in prefixes:
+        net = ipaddress.ip_network(cidr)
+        for ip in net.hosts():
+            all_ips.add(str(ip))
+
+    return all_ips
+
+
+def write_list(items, output_file):
     print(f"[+] Writing {output_file}...")
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
     with open(output_file, "w") as f:
-        for cidr in sorted(prefixes):
-            f.write(cidr + "\n")
+        for line in sorted(items):
+            f.write(line + "\n")
 
     print(f"[✓] Done! File generated : {output_file}")
 
 
+def ask_user_choice():
+    print("What do you want to extract?")
+    print("[1] All IP addresses (expand CIDRs) NOTE: This option currently uses up to 11 GB of RAM to run")
+    print("[2] Keep the original CIDR subnets")
+    while True:
+        choice = input("> ").strip()
+        if choice in ("1", "2"):
+            return choice
+        print("Please enter 1 or 2.")
+
+
 def main():
+    choice = ask_user_choice()
     data = download_amazon_ranges()
     prefixes = extract_prefixes(data)
-    write_prefixes(prefixes, OUTPUT_FILE)
+
+    if choice == "1":
+        result = expand_ips(prefixes)
+        OUTPUT_FILE = os.path.join(OUTPUT_FOLDER, "amazon_ip_ranges_extended.txt")
+    else:
+        result = prefixes
+        OUTPUT_FILE = os.path.join(OUTPUT_FOLDER, "amazon_ip_ranges.txt")
+
+    write_list(result, OUTPUT_FILE)
 
 
 if __name__ == "__main__":
